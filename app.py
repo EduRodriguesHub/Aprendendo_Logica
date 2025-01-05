fae158f25d88eb95441351ba26beabdc262b96c1
Eduardo <lordsanvisanvi@gmail.com>
2025-01-05 18:24:40 -0300

    mais alguns exercicios

--- app.py
+++ app.py
@@ -21,8 +21,6 @@ salario = horasTrabalhadas * valorPorHora
 
 print("NUMBER =", numeroFuncionario)
 print(f"SALARY = U$ {salario:.2f}")'''
-
-
 '''A, B, C = map(float, input().split())
 area_triangulo = (A * C) / 2
 area_circulo = 3.14159 * (C ** 2)
@@ -35,29 +33,21 @@ print(f"CIRCULO: {area_circulo:.3f}")
 print(f"TRAPEZIO: {area_trapezio:.3f}")
 print(f"QUADRADO: {area_quadrado:.3f}")
 print(f"RETANGULO: {area_retangulo:.3f}")'''
-
-
 '''a, b, c = map(int, input().split())
 maior = (a+b+abs(a-b))/2
 if c > maior:
     maior = c
 print(f'{int(maior)} eh o maior')'''
-
-
 '''from math import sqrt
 x1, y1 = map(float, input().split())
 x2, y2 = map(float, input().split())
 
 distancia = sqrt(((x2 - x1)**2)+((y2 - y1)**2))
 print(f'{distancia:.4f}')'''
-
-
 '''distanciaTotal = int(input())
 totalCombustivelG = float(input())
 kmPh = distanciaTotal / totalCombustivelG
 print(f'{kmPh:.3f} km/l')'''
-
-
 '''nome = input()
 salarioF = float(input())
 vendas = float(input())
@@ -67,19 +57,13 @@ print(f'TOTAL = R$ {total:.2f}')'''
 '''raio = float(input())
 area = 3.14159*(raio**2)
 print(f'A={area:.4f}')'''
-
-
 '''A = int(input())
 B = int(input())
 soma = A + B
 print(f'SOMA = {soma}')'''
-
-
 '''x = int(input())
 tempo = x * 2
 print(f'{tempo} minutos')'''
-
-
 '''n = 1000000
 while n >= 1000000:
     n = float(input())
@@ -107,58 +91,40 @@ for moeda in moedas:
     if c < len(moedas):
         c += 1
     totMoedas = 0'''
-
-
 '''a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
 diferenca = a * b - c * d
 print(f'DIFERENCA = {diferenca}')'''
-
-
 '''lista = [1, 2, 3, 4]
 lista = list(map(lambda x: x + 2, lista))
 print(lista)'''
-
-
 '''v1 = int(input())
 v2 = int(input())
 prod = v1 * v2
 print(f'PROD = {prod}')'''
-
-
 '''a, A = float(input()), 3.5
 b, B = float(input()), 7.5
 media = ((A * a) + (B * b)) / 11
 print(f'MEDIA = {media:.5f}')'''
-
-
 '''cp1, np1, vu1 = map(float, input().split())
 cp2, np2, vu2 = map(float, input().split())
 valor = (np1 * vu1) + (np2 * vu2)
 print(f'VALOR A PAGAR: R$ {valor:.2f}')'''
-
-
 '''a = float(input())
 b = float(input())
 c = float(input())
 media = ((a * 2) + (b * 3) + (c * 5)) / 10
 print(f'MEDIA = {media:.1f}')'''
-
-
 '''raio = float(input())
 volume = (4/3) * 3.14159 * (raio ** 3)
 print(f'VOLUME = {volume:.3f}')'''
-
-
 '''tempoViagem = int(input())
 velocidadeMedia = int(input())
 distancia = tempoViagem * velocidadeMedia
 litrosNecessarios = distancia / 12
 print(f'{litrosNecessarios:.3f}')'''
-
-
 '''valor = 1000000
 while valor >= 1000000:
     valor = int(input())
@@ -182,8 +148,6 @@ for tempo in tempos:
     lista.append(cont)
     cont = 0
 print(f'{lista[0]}:{lista[1]}:{lista[2]}')'''
-
-
 '''n = int(input())
 tempos = [3600, 60, 1]
 resultado = []
@@ -193,3 +157,31 @@ for tempo in tempos:
     n %= tempo  # Atualiza o restante dos segundos
 
 print(f'{resultado[0]:02}:{resultado[1]:02}:{resultado[2]:02}')'''
+'''a, b, c, d = map(int, input().split())
+if b > c and d > a and (c + d) > (a + b) and c > 0 and d > 0 and a % 2 == 0:
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')'''
+'''from math import sqrt
+a, b, c = map(float, input().split())
+if a <= 0 or (b**2 - 4 * a * c) < 0:
+    print('Impossivel calcular')
+else: 
+    r1 = (-b + sqrt(b**2 - 4 * a * c)) / (2 * a)
+    r2 = (-b - sqrt(b**2 - 4 * a * c)) / (2 * a)
+    print(f'R1 = {r1:.5f}')
+    print(f'R2 = {r2:.5f}')'''
+'''user = float(input())
+
+lista =  [[0,25], [25,50], [50,75], [75,100]]
+if user > 100 or user < 0:
+    print('Fora de intervalo')
+else:
+    for valor in lista:
+        if user <= valor[1] and valor[1] == 25:
+            print(f'Intervalo {valor[0]},{valor[1]}')
+            break
+        elif valor[0] >= 25:
+            if user <= valor[1]:
+                print(f'Intervalo ({valor[0]},{valor[1]}]')
+                break'''
